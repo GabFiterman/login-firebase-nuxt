062849f9c22a7afd6d082f7a0b167b81ef1dd1ea
Gab Fiterman <gfiterman96@gmail.com>
2022-08-23 22:04:19 -0300

    feat: Created index page, removed unnecessary files

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="Breadcrumbs">
+        <v-breadcrumbs :items="items">
+            <template v-slot:divider>
+                <v-icon>mdi-chevron-right</v-icon>
+            </template>
+        </v-breadcrumbs>
+    </div>
+</template>
+
+<script>
+export default {
+name: "Breadcrumbs"
+}
+</script>
+
+<style scoped>
+#Breadcrumbs{
+    background-color: lightslategray;
+    min-height: 5rem;
+}
+</style>
---
+++ components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-card class="mx-auto" max-width="40rem" outlined>
+        <v-list-item three-line>
+            <v-list-item-content>
+
+                <!-- Upper Tag -->
+                <div class="text-overline mb-4">
+                    {{ upperTag }}
+                </div>
+
+                <!-- Title -->
+                <v-list-item-title class="text-h5 mb-1">
+                    {{ title }}
+                </v-list-item-title>
+
+                <!-- Subtitle -->
+                <v-list-item-subtitle>
+                    {{ subtitle }}
+                </v-list-item-subtitle>
+
+            </v-list-item-content>
+
+            <!-- <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar> -->
+        </v-list-item>
+
+        <v-card-actions>
+            <v-btn outlined rounded text>
+                {{ buttonText }}
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        upperTag: String,
+        title: String,
+        subtitle: String,
+        buttonText: String
+    }
+}
+</script>
